0188ed8d6743a0ee8042c9af2f58d525e88f5392
MaiXuanChuc <43167702+MaiXuanChuc@users.noreply.github.com>
2019-12-16 19:54:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -68,8 +68,8 @@ peer.on('call', call => {
     openStream()
         .then(stream => {
             call.answer(stream);
-            playStream('localStream', stream);
-            call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+            //playStream('localStream', stream);
+            call.on('stream', remoteStream => playStream('localStream', remoteStream));
         });
 });
 
